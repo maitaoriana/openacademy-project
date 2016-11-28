52b3015950a86768362d23c14ad99b3f503b1cdd
Oriana Maita <oriana@vauxoo.com>
2016-11-28 16:09:03 +0000

    [RED] openacademy: Add _()

--- openacademy/model/openacademy_course.py
+++ openacademy/model/openacademy_course.py
@@ -1,4 +1,4 @@
-from openerp import api, models, fields, api
+from openerp import api, models, fields, api, _
 
 '''
 This module create model of Course
@@ -24,11 +24,11 @@ class Course(models.Model):
     _sql_constraints = [
         ('name_description_check',
          'CHECK(name != description)',
-         "The title of the course should not be the description"),
+         _("The title of the course should not be the description")),
 
         ('name_unique',
          'UNIQUE(name)',
-         "The course title must be unique"),
+         _("The course title must be unique")),
     ]
 
     @api.multi
@@ -38,9 +38,9 @@ class Course(models.Model):
         copied_count = self.search_count(
             [('name', '=like', u"Copy of {}%".format(self.name))])
         if not copied_count:
-            new_name = u"Copy of {}".format(self.name)
+            new_name = _(u"Copy of {}").format(self.name)
         else:
-            new_name = u"Copy of {} ({})".format(self.name, copied_count)
+            new_name = _(u"Copy of {} ({})").format(self.name, copied_count)
 
         default['name'] = new_name
         return super(Course, self).copy(default)
