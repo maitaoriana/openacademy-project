6c0255bc9bcf1a7deeaafe13f295e27d9c975577
Oriana Maita <oriana@vauxoo.com>
2016-11-30 20:20:39 +0000

    [FIX] openacademy: fix flake8 openacademy_session.py

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -6,29 +6,31 @@ from openerp import api, exceptions, fields, models, _
 class Session(models.Model):
     _name = 'openacademy.session'
 
-    name = fields.Char(required = True)
-    start_date = fields.Date(default= fields.Date.today)
+    name = fields.Char(required=True)
+    start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
     instructor_id = fields.Many2one("res.partner",
                                     string="Instructor",
                                     domain=['|',
-                                         ('instructor', '=', True),
-                                         ('category_id.name',
-                                          'ilike', "Teacher"),
-                                    ])
+                                            ('instructor', '=', True),
+                                            ('category_id.name',
+                                             'ilike', "Teacher"), ])
     course_id = fields.Many2one("openacademy.course",
-                                 ondelete="cascade",
-                                 string="Course", required=True)
+                                ondelete="cascade",
+                                string="Course",
+                                required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
     active = fields.Boolean(default=True)
 
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
 
-    end_date = fields.Date(string="End Date", store=True,
-        compute='_get_end_date', inverse='_set_end_date')
+    end_date = fields.Date(string="End Date",
+                           store=True,
+                           compute='_get_end_date',
+                           inverse='_set_end_date')
 
     hours = fields.Float(string="Duration in hours",
                          compute='_get_hours', inverse='_set_hours')
@@ -36,11 +38,10 @@ class Session(models.Model):
     attendees_count = fields.Integer(
         string="Attendees count", compute='_get_attendees_count', store=True)
 
-    state = fields.Selection([
-                              ('draft', "Draft"),
+    state = fields.Selection([('draft', "Draft"),
                               ('confirmed', "Confirmed"),
                               ('done', "Done"),
-                             ], default='draft')
+                              ], default='draft')
 
     @api.multi
     def action_draft(self):
@@ -62,15 +63,15 @@ class Session(models.Model):
             else:
                 r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
 
-
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
         if self.seats < 0:
             return {
                 'warning': {
                     'title': _("Incorrect 'seats' value"),
-                    'message': _("The number of available seats may not be negative"),
-                },
+                    'message': _("The number of available"
+                                 " seats may not be negative"),
+                    },
             }
         if self.seats < len(self.attendee_ids):
             return {
@@ -98,7 +99,8 @@ class Session(models.Model):
             if not (r.start_date and r.end_date):
                 continue
 
-            # Compute the difference between dates, but: Friday - Monday = 4 days,
+            # Compute the difference between dates,
+            # but: Friday - Monday = 4 days,
             # so add one day to get 5 days instead
             start_date = fields.Datetime.from_string(r.start_date)
             end_date = fields.Datetime.from_string(r.end_date)
@@ -121,5 +123,6 @@ class Session(models.Model):
     @api.one
     @api.constrains('instructor_id', 'attendee_ids')
     def _check_instructor_not_in_attendees(self):
-        if self.instructor_id and self.instructor_id in self.attendee_ids:
-            raise exceptions.ValidationError(_("A session's instructor can't be an attendee"))
+        if (self.instructor_id and self.instructor_id in self.attendee_ids):
+            raise exceptions.ValidationError(_("A session's instructor can't"
+                                               " be an attendee"))
